1505abe00bb9deb7c6b0bf2f73e3d59912fefbae
clopyright <akis@praxi.com.gr>
2024-05-04 11:58:17 +0300

    Welcome message when player logging to game

--- Challenge-Mode_Helper.lua
+++ Challenge-Mode_Helper.lua
@@ -7,7 +7,9 @@ function f:OnEvent(event, addOnName)
             CmHelperDB = CmHelperDB or CopyTable(defaults)
             self.db = CmHelperDB
             self:InitializeOptions()
-            -- Rest of your code
+            -- Inform the player about the addon's purpose
+            print("|cff00ccffChallenge-Mode Helper loaded. An essential addon for Mist of Pandaria Challenge Modes! Enhance your performance with valuable insights and convenient functionalities.")
+            print("|cff00ccffType /cm or /cmhelper to access options and reset boss kill times.")
         end
     elseif event == "WORLD_STATE_TIMER_STOP" then
         OnWorldStateTimerStop() -- This calls the globally shared function
@@ -18,46 +20,29 @@ function f:InitializeOptions()
     self.panel = CreateFrame("Frame")
     self.panel.name = "MoP CM Helper"
 
-    -- Create FontString for text
-    -- local addon_info1 = self.panel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-    -- addon_info1:SetPoint("TOPLEFT", 10, -20)
-    -- addon_info1:SetJustifyH("LEFT")
-    -- addon_info1:SetText("|cffffd700Mop CM Helper |r |cffffffffis an essential addon tailored to competitive groups striving to enhance their \nperformance in Mist of Pandaria Challenge Modes. Boasting two distinct panels, this tool offers \ncomprehensive insights crucial for refining strategies and achieving optimal times and ranks.")
-
-    -- local addon_info2 = self.panel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-    -- addon_info2:SetPoint("TOPLEFT", 10, -60)
-    -- addon_info2:SetJustifyH("LEFT")
-    -- addon_info2:SetText("|cffffffffThe primary panel displays essential metrics such as elapsed and remaining time, along with \nvaluable data on the best server and guild times. Additionally, it provides convenient functionalities \nlike effortless leader resets and accessible dungeon portals for all party members.")
-
-    -- local addon_info3 = self.panel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-    -- addon_info3:SetPoint("TOPLEFT", 10, -100)
-    -- addon_info3:SetJustifyH("LEFT")
-    -- addon_info3:SetText("|cffffffffMeanwhile, the secondary panel serves as a treasure trove of information, featuring the best \nkill times and real-time updates on current run times for each dungeon boss. Armed with these \ninsights, teams can meticulously analyze their performance, identify areas for improvement, and \nrecalibrate their strategies for greater success.")
-
-    -- local addon_info4 = self.panel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-    -- addon_info4:SetPoint("TOPLEFT", 10, -150)
-    -- addon_info4:SetJustifyH("LEFT")
-    -- addon_info4:SetText("|cffffd700Mop CM Helper |r |cffffffffisn't just an addon; it's a strategic ally for those committed to mastering \nMist of Pandaria Challenge Modes and dominating the leaderboards.")
-
     local addon_info1 = self.panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
     addon_info1:SetPoint("TOPLEFT", 7, -20)
     addon_info1:SetJustifyH("LEFT")
-    addon_info1:SetText("|cffffd700Mop CM Helper |r |cffffffffis an essential addon tailored to competitive groups \nstriving to enhance their performance in Mist of Pandaria Challenge Modes. \nBoasting two distinct panels, this tool offers comprehensive insights \ncrucial for refining strategies and achieving optimal times and ranks.")
+    addon_info1:SetText(
+        "|cffffd700Mop CM Helper |r |cffffffffis an essential addon tailored to competitive groups \nstriving to enhance their performance in Mist of Pandaria Challenge Modes. \nBoasting two distinct panels, this tool offers comprehensive insights \ncrucial for refining strategies and achieving optimal times and ranks.")
 
     local addon_info2 = self.panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
     addon_info2:SetPoint("TOPLEFT", addon_info1, "BOTTOMLEFT", 0, -10)
     addon_info2:SetJustifyH("LEFT")
-    addon_info2:SetText("|cffffffffThe primary panel displays essential metrics such as elapsed and remaining \ntime, along with valuable data on the best server and guild times. \nAdditionally, it provides convenient functionalities like effortless leader \nresets and accessible dungeon portals for all party members.")
+    addon_info2:SetText(
+        "|cffffffffThe primary panel displays essential metrics such as elapsed and remaining \ntime, along with valuable data on the best server and guild times. \nAdditionally, it provides convenient functionalities like effortless leader \nresets and accessible dungeon portals for all party members.")
 
     local addon_info3 = self.panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
     addon_info3:SetPoint("TOPLEFT", addon_info2, "BOTTOMLEFT", 0, -10)
     addon_info3:SetJustifyH("LEFT")
-    addon_info3:SetText("|cffffffffMeanwhile, the secondary panel serves as a treasure trove of information, \nfeaturing the best kill times and real-time updates on current run times for \neach dungeon boss. Armed with these insights, teams can meticulously \nanalyze their performance, identify areas for improvement, and \nrecalibrate their strategies for greater success.")
+    addon_info3:SetText(
+        "|cffffffffMeanwhile, the secondary panel serves as a treasure trove of information, \nfeaturing the best kill times and real-time updates on current run times for \neach dungeon boss. Armed with these insights, teams can meticulously \nanalyze their performance, identify areas for improvement, and \nrecalibrate their strategies for greater success.")
 
     local addon_info4 = self.panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
     addon_info4:SetPoint("TOPLEFT", addon_info3, "BOTTOMLEFT", 0, -10)
     addon_info4:SetJustifyH("LEFT")
-    addon_info4:SetText("|cffffd700Mop CM Helper |r |cffffffffisn't just an addon; it's a strategic ally for those \ncommitted to mastering Mist of Pandaria Challenge Modes \nand dominating the leaderboards.")
+    addon_info4:SetText(
+        "|cffffd700Mop CM Helper |r |cffffffffisn't just an addon; it's a strategic ally for those \ncommitted to mastering Mist of Pandaria Challenge Modes \nand dominating the leaderboards.")
 
     local text1 = self.panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
     text1:SetPoint("TOPLEFT", 7, -420)
@@ -82,40 +67,31 @@ function f:InitializeOptions()
     imageFrame:SetPoint("TOPLEFT", 5, 0)
     imageFrame:SetSize(615, 565)
 
--- Button to Delete Saved Data
-local btn = CreateFrame("Button", nil, self.panel, "UIPanelButtonTemplate")
-btn:SetPoint("CENTER", 0, -80)
-btn:SetText("Delete Saved Boss Kill Times")
-btn:SetWidth(350)
-btn:SetHeight(50)
-btn:SetScript("OnClick", function()
-    StaticPopupDialogs["CONFIRM_DELETE_TIMES"] = {
-        text = "Are you sure you want to delete saved data for boss kill times?",
-        button1 = "Yes - Will Relog",
-        button2 = "No",
-        OnAccept = function()
-            -- Delete saved boss kill times
-            -- Your code to delete saved boss kill times goes here
-            
-            -- Logout from the game
-            Logout()
-        end,
-        timeout = 0,
-        whileDead = true,
-        hideOnEscape = true,
-        preferredIndex = 3,
-    }
-    StaticPopup_Show("CONFIRM_DELETE_TIMES")
-end)
+    -- Button to Delete Saved Data
+    local btn = CreateFrame("Button", nil, self.panel, "UIPanelButtonTemplate")
+    btn:SetPoint("CENTER", 0, -80)
+    btn:SetText("Delete Saved Boss Kill Times")
+    btn:SetWidth(350)
+    btn:SetHeight(50)
+    btn:SetScript("OnClick", function()
+        StaticPopupDialogs["CONFIRM_DELETE_TIMES"] = {
+            text = "Are you sure you want to delete saved data for boss kill times?",
+            button1 = "Yes - Will Relog",
+            button2 = "No",
+            OnAccept = function()
+                -- Delete saved boss kill times
+                -- Your code to delete saved boss kill times goes here
 
-    -- -- Create a texture for the image
-    -- local imageTexture = imageFrame:CreateTexture(nil, "OVERLAY")
-    -- imageTexture:SetTexture("Interface\\AddOns\\Challenge-Mode_Helper\\logo")
-    -- imageTexture:SetAllPoints() -- Fill the entire frame with the texture
-    -- -- Set the opacity (alpha) of the texture
-    -- imageTexture:SetAlpha(0.2) -- 10% opacity
-
-
+                -- Logout from the game
+                Logout()
+            end,
+            timeout = 0,
+            whileDead = true,
+            hideOnEscape = true,
+            preferredIndex = 3
+        }
+        StaticPopup_Show("CONFIRM_DELETE_TIMES")
+    end)
 
     InterfaceOptions_AddCategory(self.panel)
 end
