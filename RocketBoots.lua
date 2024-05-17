1ae5432b3a160d0585abaac1423be5b92c66ec82
Kontopidis Giorgos <kontopidisg@unisystems.gr>
2024-05-17 08:38:22 +0300

    auto equip rocket boots

---
+++ RocketBoots.lua
@@ -0,0 +1,26 @@
+-- RocketBoots.lua
+
+local f = CreateFrame("Frame")
+local bootItemID = 35581 -- The item ID for Rocket Boots Xtreme
+
+local function EquipRocketBoots()
+    if IsInInstance() then
+        local scenarioName, _, _, difficultyName = GetInstanceInfo()
+            if difficultyName == "Challenge Mode" then
+            for bag = 0, NUM_BAG_SLOTS do
+                for slot = 1, GetContainerNumSlots(bag) do
+                    local itemID = GetContainerItemID(bag, slot)
+                    if itemID == bootItemID then
+                        UseContainerItem(bag, slot)
+                        print("Rocket Boots Xtreme equipped!")
+                        return
+                    end
+                end
+            end
+        end
+    end
+end
+
+f:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:SetScript("OnEvent", EquipRocketBoots)
